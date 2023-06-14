ceff773acb227aa6f4556568c621b214ac517b1c
kanaaa224 <kanaaa224@outlook.com>
2023-06-14 14:18:18 +0900

    Initial commit

---
+++ SourceFiles/main.cpp
@@ -0,0 +1,51 @@
+﻿#include "DxLib.h"
+#include "main.h"
+
+int Resources::snd_bgm[5];
+int Resources::snd_se[5];
+
+/********************************
+* メインプログラム 開始
+********************************/
+int WINAPI WinMain(_In_ HINSTANCE  hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR LpCmdLine, _In_ int NCmdShow) {
+
+    SetMainWindowText(GAME_NAME);                  // ウィンドウタイトルを設定
+    SetMainWindowClassName(GAME_NAME);             // 他のDxLibと競合しないようにウィンドウクラスを設定
+    ChangeWindowMode(TRUE);                        // ウインドウモードで起動
+    SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32); // ウインドウのサイズ
+    if (DxLib_Init() == -1) return -1;             // DXライブラリの初期化処理
+    SetDrawScreen(DX_SCREEN_BACK);                 // 描画先画面を裏にする（ダブルバッファリング）
+
+    // タイトル シーンオブジェクト作成
+    //SceneManager* sceneMng = new SceneManager((AbstractScene*) new Title());
+
+    //Fps fps;
+
+    // 一部のリソースをstaticとして読み込み
+    int bgm_title, bgm_result;
+    if ((bgm_title = LoadSoundMem("Resources/Sounds/bgm_title.wav")) == -1) return -1;
+    ChangeVolumeSoundMem(255, bgm_title);
+    Resources::Set(SND, BGM, TITLE, bgm_title);
+    if ((bgm_result = LoadSoundMem("Resources/Sounds/bgm_result.wav")) == -1) return -1;
+    ChangeVolumeSoundMem(255, bgm_result);
+    Resources::Set(SND, BGM, 2, bgm_result);
+
+    // ゲームループし、シーンマネジャーでシーンの更新
+    while ((ProcessMessage() == 0)/* && (sceneMng->Update() != nullptr) && !(InputControl::OnButton(XINPUT_BUTTON_BACK))*/) {
+        ClearDrawScreen(); // 画面の初期化
+
+        // シーンマネジャーでシーンの描画開始
+        //sceneMng->Draw();
+
+        //fps.Update();
+        //fps.Draw();
+        //InputControl::Update(); //コントローラー入力更新
+        
+        ScreenFlip(); // 裏画面の内容を表画面に反映する
+        //fps.Wait();		//待機
+
+    };
+
+    DxLib_End(); // DXライブラリ使用の終了処理
+    return 0;    // プログラムの終了
+};
---
+++ SourceFiles/main.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define GAME_NAME "バルーンファイト"
+
+#include <stdio.h>
+#include <math.h>
+#include <string>
+using std::string;
+using std::to_string;
+#include <time.h>
+//#include "SceneManager.h"
+
+//#include "PadInput.h"
+
+#include "DxLib.h"
+
+// 各シーンのヘッダーファイル
+//#include "Title.h"
+//#include "GameMainScene.h"
+//#include "Result.h"
+//#include "Help.h"
+//#include "Ranking.h"
+//#include "DrawRanking.h"
+//#include "InputRanking.h"
+//#include "Credit.h"
+
+// リソース管理
+#define IMG 0
+#define SND 1
+
+#define BGM 0
+#define SE 1
+
+#define TITLE 0
+#define MAIN 1
+
+class Resources {
+private:
+    static int snd_bgm[];
+    static int snd_se[];
+public:
+    static int Get(int media, int type, int getNum) {
+        if (media == 1) {
+            if (type == 0) {
+                return snd_bgm[getNum];
+            }
+            else if (type == 1) {
+                return snd_se[getNum];
+            };
+        }
+    };
+    static void Set(int media, int type, int getNum, int data) {
+        if (media == 1) {
+            if (type == 0) {
+                snd_bgm[getNum] = data;
+            }
+            else if (type == 1) {
+                snd_se[getNum] = data;
+            };
+        }
+    };
+};
