303aa4df7542b384c250adfb1b3cc0d3032e5dd4
kanaaa224 <kanaaa224@outlook.com>
2023-06-16 14:57:05 +0900

    Update

--- SourceFiles/gamePlayer.cpp
+++ SourceFiles/gamePlayer.cpp
@@ -8,6 +8,15 @@
 GamePlayer::GamePlayer() {
 	// 初期化処理
 	state = 0;
+
+	// 画像読み込み
+	if ((img_bg = LoadGraph("Resources/Images/kari.png")) == -1) throw "Resources/Images/kari.png";
+
+	w = 30;
+	h = 60;
+
+	x = SCREEN_WIDTH / 2;
+	y = 270;
 };
 
 // プレイヤークラスのデストラクタ
@@ -22,5 +31,9 @@ void GamePlayer::Update() {
 
 // プレイヤー描画
 void GamePlayer::Draw() const {
+	// 背景表示
+	DrawExtendGraph(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, img_bg, TRUE);
+
+	DrawBox(x, y, x + w, y + h, 0xffffff, FALSE);
 	DrawFormatString(20, 100, 0xffffff, "プレイヤークラス実行中！");
 };
