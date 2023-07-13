fab828e9f247f984cae87a60f4b6f4611d43fe1e
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-07-13 12:12:58 +0900

    ゲームステージの中の当たり判定の削除

--- SourceFiles/GameStage.h
+++ SourceFiles/GameStage.h
@@ -11,11 +11,6 @@ private:
 	int img_bg;
 	int img_bg1;
 	int cloudimg[2];//雲の画像用
-	int StageFlg;
-	int mx0, mx1, my0, my1, sx0, sx1, sy0, sy1;
-	int StageX, StageY;
-	int playerX,playerY;
-	float PlayerAngle;
 
 public:
 	// コンストラクタ
