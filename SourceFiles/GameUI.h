b0cb033aedd2682c8eaa82aeaef19268146aeced
kanaaa224 <kanaaa224@outlook.com>
2023-07-14 18:02:36 +0900

    new : ゲームUI開発開始

---
+++ SourceFiles/GameUI.h
@@ -0,0 +1,24 @@
+﻿/********************************
+* ゲームUI
+* 担当：島袋
+********************************/
+#pragma once
+#include "main.h"
+
+// プレイヤークラス
+class GameUI {
+private:
+
+public:
+	// コンストラクタ
+	GameUI();
+
+	// デストラクタ
+	~GameUI();
+
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw() const;
+};
