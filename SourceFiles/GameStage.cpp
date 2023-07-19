2433ae4b9a3f2d1beedaa16b00528746a7e2ec3d
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2023-07-19 14:39:04 +0900

    ステージ２の描画

--- SourceFiles/GameStage.cpp
+++ SourceFiles/GameStage.cpp
@@ -4,15 +4,18 @@
 ********************************/
 #include "main.h"
 
+int GameStage::Stage=1;
+
 GameStage::GameStage() {
 	// 初期化処理
 	
 	// 画像読み込み
 	img_sea = LoadGraph("Resources/Images/Stage/Stage_Sea01.png");
-	img_footing = LoadGraph("Resources/Images/Stage/Stage_Footing01.png");
+	img_footing[0] = LoadGraph("Resources/Images/Stage/Stage_Footing01.png");
+	img_footing[1] = LoadGraph("Resources/Images/Stage/Stage_Footing02.png");
 	LoadDivGraph("Resources/Images/Stage/Stage_CloudAnimation.png", 3, 3, 1, 128, 64, img_cloud);
 
-	// 仮 - 足場の当たり判定用ボックス
+	// 仮ステージ１ - 足場の当たり判定用ボックス
 	block[0][0] = 180; // 真ん中
 	block[0][1] = 250;
 	block[0][2] = 460;
@@ -43,12 +46,23 @@ void GameStage::Draw() const {
 
 	// 足場
 	//DrawGraph(-50, 430, img_bg1, FALSE);
-	DrawGraph(180, 250, img_footing, TRUE); // 真ん中
-	DrawGraph(-50, 430, img_footing, TRUE); // 左下
-	DrawGraph(370, 430, img_footing, TRUE); // 右下
+	//仮ステージ１の表示
+	if (Stage == 0) {
+		DrawGraph(180, 250, img_footing[0], TRUE); // 真ん中
+		DrawGraph(-50, 430, img_footing[0], TRUE); // 左下
+		DrawGraph(370, 430, img_footing[0], TRUE); // 右下
 
-	DrawGraph(320, 80, img_cloud[0], TRUE);  // 雲
+		DrawGraph(320, 80, img_cloud[0], TRUE);  // 雲
+	}
+	if (Stage == 1) {
+		DrawGraph(180, 250, img_footing[0], TRUE); // 真ん中
+		DrawGraph(-50, 430, img_footing[0], TRUE); // 左下
+		DrawGraph(370, 430, img_footing[0], TRUE); // 右下
+		DrawGraph(80, 100, img_footing[1], TRUE);
+		DrawGraph(450, 100, img_footing[1], TRUE);
 
+		DrawGraph(320, 80, img_cloud[0], TRUE);  // 雲
+	}
 	// 仮 - 足場の当たり判定用ボックス
 	//DrawBox((int)block[0][0], (int)block[0][1], (int)block[0][2], (int)block[0][3], 0xffffff, FALSE); // 真ん中
 	//DrawBox((int)block[1][0], (int)block[1][1], (int)block[1][2], (int)block[1][3], 0xffffff, FALSE); // 左下
