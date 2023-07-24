81f104fc3834024c729615c73cdd11935018b247
yafusosyun <122337129+yafusosyun@users.noreply.github.com>
2023-07-24 11:43:43 +0900

    a

--- SourceFiles/GameStageGimmick.cpp
+++ SourceFiles/GameStageGimmick.cpp
@@ -1,68 +1,65 @@
 ﻿/********************************
-* ゲームステージ処理
+* ゲームステージギミック処理
 * 編集者：
 ********************************/
 #include "main.h"
 
+GameStageGimmick::GameStageGimmick() {
+	// シャボン玉画像読込
+	LoadDivGraph("Resources/Images/Stage/Stage_BubbleAnimation.png", 4, 4, 1, 64, 64, img_bubble);
 
-GameStageGimmick::GameStageGimmick(int* img_bubble)
-{
-
-	// 画像ポインタを受け取る処理
-	for (int i = 0; i < 3; i++) {
-		img_bubble[i] = img_bubble[i];
-	}
-
-	// 初期化処理
-	bubble_width = 200;
 	frequency = 0;
 	bubble_count = 0;
 	frameCounter = 0;
-	bubble_anm = 0;
+	bubble_anim = 0;
 
+	bubble_height = 400;
 
-	// 画像読み込み//シャボン玉画像読込
-	LoadDivGraph("Resources/Images/Stage/Stage_BubbleAnimation.png", 4, 4, 1, 64, 64, img_bubble);
+	//シャボン玉スコア画像　仮
+	//GetScore_500 = LoadGraph("Resources/Images/Stage/GetScore_500.png");
+};
+
+GameStageGimmick::~GameStageGimmick() {
+
+};
+
+void GameStageGimmick::BubbleUpdate() {
 	
-
-	//
-	//bubble_width = 200;
-	//bubble_height = 400;
-}
-
-GameStageGimmick::~GameStageGimmick()
-{
-	// 終了処理
-}
-
-void GameStageGimmick::bubble_Update()
-{
 	float amplitude = 50.0f;
 	float frequency = 0.02;
+	
+	float bubble_x = 0;
 
-	float bubble_x = 200;
+	if (CheckHitKey(KEY_INPUT_B)) {
+		
+		bubble_x += amplitude * sin(frequency * bubble_count);
+		//amplitude波の大きさを決める　//frequency　1秒あたりに何回周期するかを決める
+		bubble_width = bubble_x;
+		bubble_count += 1;
+		bubble_height -= 0.5;
 
-	bubble_x += amplitude * sin(frequency * bubble_count);
-	//amplitude波の大きさを決める　//frequency　1秒あたりに何回周期するかを決める
-	bubble_width = bubble_x;
-	bubble_count += 1;
-	bubble_height -= 0.5;
+		frameCounter++;
+		if (frameCounter % 20 == 0) {
+			if (bubble_anim == 0) bubble_anim = 1;
+			else if (bubble_anim == 1) bubble_anim = 2;
+		};
+		if (frameCounter % 60 == 0) {
+			frameCounter = 0;
+			bubble_anim = 0;
+		};
+	}
+};
+	
 
-	frameCounter++;
-	if (frameCounter % 20 == 0) {
-		if (bubble_anm == 0) bubble_anm = 1;
-		else if (bubble_anm == 1) bubble_anm = 2;
-	};
-	if (frameCounter % 60 == 0) {
-		frameCounter = 0;
-		bubble_anm = 0;
-	};
-}
+void GameStageGimmick::BubbleDraw() const {
 
-void GameStageGimmick::bubble_Draw() const
-{
-	DrawFormatString(100, 110, 0x00ffff, "%d", bubble_anm);
+	DrawGraph(bubble_width, bubble_height, img_bubble[bubble_anim], TRUE);  // シャボン
+
+	// 仮 - 足場の当たり判定用ボックス
+	//DrawBox((int)block[0][0], (int)block[0][1], (int)block[0][2], (int)block[0][3], 0xffffff, FALSE); // 真ん中
+	//DrawBox((int)block[1][0], (int)block[1][1], (int)block[1][2], (int)block[1][3], 0xffffff, FALSE); // 左下
+	//DrawBox((int)block[2][0], (int)block[2][1], (int)block[2][2], (int)block[2][3], 0xffffff, FALSE); // 右下
+
+	DrawFormatString(100, 110, 0x00ffff, "%d", bubble_anim);
 	DrawFormatString(100, 120, 0x00ffff, "%d", frameCounter);
-
-	DrawGraph(bubble_width, bubble_height, img_bubble[bubble_anm], TRUE);  // シャボン
-}
+};
--- SourceFiles/GameStageGimmick.h
+++ SourceFiles/GameStageGimmick.h
@@ -1,5 +1,5 @@
 ﻿/********************************
-* ゲームステージ処理
+* ゲームステージギミック処理
 * 編集者：
 ********************************/
 #pragma once
@@ -8,34 +8,43 @@
 // ステージクラス
 class GameStageGimmick {
 private:
-	//int img_sea;      // 海
-	//int img_footing;  // 足場
-	//int img_cloud[2]; // 雲
+	int img_bubble[4]; // シャボン
 
-	int img_bubble[3];//シャボン
 	int bubble_height;
 	int bubble_width;
-	int frequency;//シャボン玉のの周波数
+	int frequency; // シャボン玉のの周波数
 	int  bubble_count;
 	float amplitude;
 	float bubble_x;
-	//シャボン玉のアニメーション用
+
+	// シャボン玉のアニメーション用
 	int frameCounter;
-	int bubble_anm;
+	int bubble_anim;
+
+	int bubble_flg;
+
+	//int GetScore_500;//仮りで画像表示するための変数
 
 public:
 	// コンストラクタ
-	GameStageGimmick(int *bubble_images);
+	GameStageGimmick();
 
 	// デストラクタ
 	~GameStageGimmick();
 
 	// 更新
-	void bubble_Update();
+	void Update() {
+		BubbleUpdate();
+	};
 
 	// 描画
-	void bubble_Draw() const;
+	void Draw() const {
+		BubbleDraw();
+	};
 
+	// シャボン玉 更新
+	void BubbleUpdate();
 
-
+	// シャボン玉 描画
+	void BubbleDraw() const;
 };
