3490f028a1c372b08191a2dab49f7be77234c4d7
L4ntaarn <sho-nagamine-k22h@stu.kbc.ac.jp>
2023-07-27 13:45:06 +0900

    魚

--- SourceFiles/GameEnemy.h
+++ SourceFiles/GameEnemy.h
@@ -5,25 +5,23 @@
 #pragma once
 #include "main.h"
 
-// 敵 構造体
-struct Enemy {
-	Position position;
-	Size size;
-	int state = 0;
-	int hp = 0;
-};
+
 class GameEnemy {
 private:
 	int frameCounter;
 
-	Enemy enemy;
+	Person enemy;
+	
 
 	int anim;
 
 	int r_enemy[18];
 	int g_enemy[18];
 	int p_enemy[18];
-	int fish[10];
+	
+
+	// プレイヤーの左上・右上座標
+	Collide playerCollide;
 
 public:
 	// コンストラクタ
@@ -47,5 +45,51 @@ public:
 
 	int GetEnemyState() {
 		return enemy.state;
+	}
+	// プレイヤーの衝突座標を設定
+	void SetPlayerCollide(Collide collide) {
+		playerCollide = collide;
+	};
+};
+class EnemyFish {
+private:
+	int fish[10];
+	int Flg;
+	int x;
+	int y;
+	int anim;
+	int frameCounter;
+
+public:
+
+	EnemyFish() {
+		Flg = 0;
+		anim = 0;
+		LoadDivGraph("Resources/Images/Enemy/Enemy_FishAnimation.png", 10, 5, 2, 64, 64, fish);
+	};
+	~EnemyFish() {
+		for (int i = 0; i < 10; i++) {
+			DeleteGraph(fish[i]);
+		}
+	};
+
+	void Update() {
+		x = 300;
+		y = SCREEN_HEIGHT - 50;
+		if (Flg) {
+			if (frameCounter % 5 == 0) {
+				if (anim < 3) {
+					anim++;
+				}
+			}
+		}
+	};
+
+	void Draw() const {
+		DrawRotaGraph(x, y, 1.0f, 0, fish[anim], TRUE);
+	};
+	void Spawn() {
+		Flg = 1;
+
 	}
 };
