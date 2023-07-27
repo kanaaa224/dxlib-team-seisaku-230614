55c2506b38aa054ddb54cd00ca00d02cccd5dfdc
kanaaa224 <kanaaa224@outlook.com>
2023-07-27 09:44:02 +0900

    optimize

--- SourceFiles/GamePlayer.h
+++ SourceFiles/GamePlayer.h
@@ -302,3 +302,26 @@ public:
 		};
 	};
 };
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+class Collider {
+public:
+	static int CheckCollide(Collide collideA, Collide collideB) {
+		if (collideA.lr.x <= collideB.ul.x || collideA.ul.x >= collideB.lr.x || collideA.lr.y <= collideB.ul.y || collideA.ul.y >= collideB.lr.y) return 0;
+		if (fabsf(collideA.lr.y - collideB.ul.y) < 10) return 1;
+		else if (fabsf(collideA.ul.y - collideB.lr.y) < 10) return 2;
+		else if (fabsf(collideA.lr.x - collideB.ul.x) < 10) return 3;
+		else if (fabsf(collideA.ul.x - collideB.lr.x) < 10) return 4;
+		else return 0;
+	};
+
+	static int CheckCollideBox(float s1X1, float s1Y1, float s1X2, float s1Y2, float s2X1, float s2Y1, float s2X2, float s2Y2) {
+		if (s1X2 <= s2X1 || s1X1 >= s2X2 || s1Y2 <= s2Y1 || s1Y1 >= s2Y2) return 0;
+		if (fabsf(s1Y2 - s2Y1) < 10) return 1;
+		else if (fabsf(s1Y1 - s2Y2) < 10) return 2;
+		else if (fabsf(s1X2 - s2X1) < 10) return 3;
+		else if (fabsf(s1X1 - s2X2) < 10) return 4;
+		else return 0;
+	};
+};*/
